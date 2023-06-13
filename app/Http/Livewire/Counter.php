fb1c736b8b2183f2c8b366b087fabae4891ce988
myxtraining <myxtraining@gmail.com>
2023-06-13 09:51:24 +0800

    MODULE 3: Add button plus

--- app/Http/Livewire/Counter.php
+++ app/Http/Livewire/Counter.php
@@ -6,6 +6,13 @@ use Livewire\Component;
 
 class Counter extends Component
 {
+    public $counter = 0;
+
+    public function add()
+    {
+        $this->counter++;
+    }    
+
     public function render()
     {
         return view('livewire.counter');
