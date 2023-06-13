409116169fd33bcb857ce17782d4d3e49e39bf86
myxtraining <myxtraining@gmail.com>
2023-06-13 12:30:34 +0800

    MODULE 5 : Review using database

--- app/Http/Livewire/Review.php
+++ app/Http/Livewire/Review.php
@@ -2,38 +2,44 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Review as ModelsReview;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Review extends Component
 {
     // Nanti content ni akan datang dari DB
-    public $reviews = [
-        [
-            'name' => 'Siti Aishah',
-            'content' => "This is useful for persisting search filters, 
-            active tabs, and other features where users will be frustrated if 
-            their configuration is reset after refreshing or leaving and revisiting a page."
-        ],
-        [
-            'name' => 'Siti Khadijah',
-            'content' => "You can include the CDN build of this plugin as a <script> 
-            tag, just make sure to include it BEFORE Alpine's core JS file."
-        ],
-        [
-            'name' => 'Nurhaliza Abdullah',
-            'content' => "You can use this plugin by either including it from a 
-            <script> tag or installing it via NPM:"
-        ],
-    ];
+    // public $reviews = [
+    //     [
+    //         'name' => 'Siti Aishah',
+    //         'content' => "This is useful for persisting search filters, 
+    //         active tabs, and other features where users will be frustrated if 
+    //         their configuration is reset after refreshing or leaving and revisiting a page."
+    //     ],
+    //     [
+    //         'name' => 'Siti Khadijah',
+    //         'content' => "You can include the CDN build of this plugin as a <script> 
+    //         tag, just make sure to include it BEFORE Alpine's core JS file."
+    //     ],
+    //     [
+    //         'name' => 'Nurhaliza Abdullah',
+    //         'content' => "You can use this plugin by either including it from a 
+    //         <script> tag or installing it via NPM:"
+    //     ],
+    // ];
 
     public $newReview;
 
     public function save()
     {
-        array_unshift($this->reviews, [
-            'name' => Auth::user()->name,
-            'content' => $this->newReview
+        // array_unshift($this->reviews, [
+        //     'name' => Auth::user()->name,
+        //     'content' => $this->newReview
+        // ]);
+
+        ModelsReview::create([
+            'content' => $this->newReview,
+            'user_id' => Auth::id()
         ]);
         
         $this->newReview = '';
@@ -42,6 +48,8 @@ class Review extends Component
 
     public function render()
     {
-        return view('livewire.review');
+        return view('livewire.review', [
+            'reviews' => ModelsReview::all()
+        ]);
     }
 }
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['content', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
