062e5091f5f13bb9174d2eb941666dd353b96b49
myxtraining <myxtraining@gmail.com>
2023-06-13 11:55:20 +0800

    MODULE 5 : Review using array

--- resources/views/livewire/review.blade.php
+++ resources/views/livewire/review.blade.php
@@ -14,7 +14,7 @@
                     <!-- Form -->
                     <div style="max-width:1200px;">
 
-                        <form class="flex items-center">
+                        <form class="flex items-center" wire:submit.prevent="save">
                             <label for="voice-search" class="sr-only">Search</label>
                             <div class="relative w-full">
                                 <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
@@ -25,7 +25,7 @@
                                             clip-rule="evenodd"></path>
                                     </svg>
                                 </div>
-                                <input type="text" id="voice-search"
+                                <input type="text" id="voice-search" wire:model="newReview"
                                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                     placeholder="Search Mockups, Logos, Design Templates..." required="">
                                 <button type="button" class="flex absolute inset-y-0 right-0 items-center pr-3">
@@ -52,115 +52,29 @@
 
                     <!-- List -->
                     <ul role="list" class="divide-y divide-gray-100">
-                        <li class="flex justify-between gap-x-6 py-5">
-                            <div class="flex gap-x-4">
-                                <img class="h-12 w-12 flex-none rounded-full bg-gray-50"
-                                    src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                    alt="">
-                                <div class="min-w-0 flex-auto">
-                                    <p class="text-sm font-semibold leading-6 text-gray-900">Leslie Alexander</p>
-                                    <p class="mt-1 truncate text-xs leading-5 text-gray-500">
-                                        leslie.alexander@example.com</p>
-                                </div>
-                            </div>
-                            <div class="hidden sm:flex sm:flex-col sm:items-end">
-                                <p class="text-sm leading-6 text-gray-900">Co-Founder / CEO</p>
-                                <p class="mt-1 text-xs leading-5 text-gray-500">Last seen <time
-                                        datetime="2023-01-23T13:23Z">3h ago</time></p>
-                            </div>
-                        </li>
-                        <li class="flex justify-between gap-x-6 py-5">
-                            <div class="flex gap-x-4">
-                                <img class="h-12 w-12 flex-none rounded-full bg-gray-50"
-                                    src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                    alt="">
-                                <div class="min-w-0 flex-auto">
-                                    <p class="text-sm font-semibold leading-6 text-gray-900">Michael Foster</p>
-                                    <p class="mt-1 truncate text-xs leading-5 text-gray-500">michael.foster@example.com
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="hidden sm:flex sm:flex-col sm:items-end">
-                                <p class="text-sm leading-6 text-gray-900">Co-Founder / CTO</p>
-                                <p class="mt-1 text-xs leading-5 text-gray-500">Last seen <time
-                                        datetime="2023-01-23T13:23Z">3h ago</time></p>
-                            </div>
-                        </li>
-                        <li class="flex justify-between gap-x-6 py-5">
-                            <div class="flex gap-x-4">
-                                <img class="h-12 w-12 flex-none rounded-full bg-gray-50"
-                                    src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                    alt="">
-                                <div class="min-w-0 flex-auto">
-                                    <p class="text-sm font-semibold leading-6 text-gray-900">Dries Vincent</p>
-                                    <p class="mt-1 truncate text-xs leading-5 text-gray-500">dries.vincent@example.com
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="hidden sm:flex sm:flex-col sm:items-end">
-                                <p class="text-sm leading-6 text-gray-900">Business Relations</p>
-                                <div class="mt-1 flex items-center gap-x-1.5">
-                                    <div class="flex-none rounded-full bg-emerald-500/20 p-1">
-                                        <div class="h-1.5 w-1.5 rounded-full bg-emerald-500"></div>
+
+                        @foreach ($reviews as $review)
+                            <li class="flex justify-between gap-x-6 py-5">
+                                <div class="flex gap-x-4">
+                                    <img class="h-12 w-12 flex-none rounded-full bg-gray-50"
+                                        src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                        alt="">
+                                    <div class="min-w-0 flex-auto">
+                                        <p class="text-sm font-semibold leading-6 text-gray-900">{{ $review['name'] }}</p>
+                                        <p class="mt-1 truncate text-xs leading-5 text-gray-500">
+                                            {{ $review['content'] }}</p>
                                     </div>
-                                    <p class="text-xs leading-5 text-gray-500">Online</p>
                                 </div>
-                            </div>
-                        </li>
-                        <li class="flex justify-between gap-x-6 py-5">
-                            <div class="flex gap-x-4">
-                                <img class="h-12 w-12 flex-none rounded-full bg-gray-50"
-                                    src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                    alt="">
-                                <div class="min-w-0 flex-auto">
-                                    <p class="text-sm font-semibold leading-6 text-gray-900">Lindsay Walton</p>
-                                    <p class="mt-1 truncate text-xs leading-5 text-gray-500">lindsay.walton@example.com
-                                    </p>
+                                <div class="hidden sm:flex sm:flex-col sm:items-end">
+                                    <p class="text-sm leading-6 text-gray-900">Co-Founder / CEO</p>
+                                    <p class="mt-1 text-xs leading-5 text-gray-500">Last seen <time
+                                            datetime="2023-01-23T13:23Z">3h ago</time></p>
                                 </div>
-                            </div>
-                            <div class="hidden sm:flex sm:flex-col sm:items-end">
-                                <p class="text-sm leading-6 text-gray-900">Front-end Developer</p>
-                                <p class="mt-1 text-xs leading-5 text-gray-500">Last seen <time
-                                        datetime="2023-01-23T13:23Z">3h ago</time></p>
-                            </div>
-                        </li>
-                        <li class="flex justify-between gap-x-6 py-5">
-                            <div class="flex gap-x-4">
-                                <img class="h-12 w-12 flex-none rounded-full bg-gray-50"
-                                    src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                    alt="">
-                                <div class="min-w-0 flex-auto">
-                                    <p class="text-sm font-semibold leading-6 text-gray-900">Courtney Henry</p>
-                                    <p class="mt-1 truncate text-xs leading-5 text-gray-500">courtney.henry@example.com
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="hidden sm:flex sm:flex-col sm:items-end">
-                                <p class="text-sm leading-6 text-gray-900">Designer</p>
-                                <p class="mt-1 text-xs leading-5 text-gray-500">Last seen <time
-                                        datetime="2023-01-23T13:23Z">3h ago</time></p>
-                            </div>
-                        </li>
-                        <li class="flex justify-between gap-x-6 py-5">
-                            <div class="flex gap-x-4">
-                                <img class="h-12 w-12 flex-none rounded-full bg-gray-50"
-                                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                    alt="">
-                                <div class="min-w-0 flex-auto">
-                                    <p class="text-sm font-semibold leading-6 text-gray-900">Tom Cook</p>
-                                    <p class="mt-1 truncate text-xs leading-5 text-gray-500">tom.cook@example.com</p>
-                                </div>
-                            </div>
-                            <div class="hidden sm:flex sm:flex-col sm:items-end">
-                                <p class="text-sm leading-6 text-gray-900">Director of Product</p>
-                                <div class="mt-1 flex items-center gap-x-1.5">
-                                    <div class="flex-none rounded-full bg-emerald-500/20 p-1">
-                                        <div class="h-1.5 w-1.5 rounded-full bg-emerald-500"></div>
-                                    </div>
-                                    <p class="text-xs leading-5 text-gray-500">Online</p>
-                                </div>
-                            </div>
-                        </li>
+                            </li>
+                        @endforeach
+
+
+
                     </ul>
 
 
